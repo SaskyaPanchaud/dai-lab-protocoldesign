be0bf38d4a79082b525f80944dd3163795aa0b0f
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-10-19 16:18:34 +0200

    wip

--- code/AlessioGiuliano_SaskyaPanchaud/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/AlessioGiuliano_SaskyaPanchaud/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,13 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4.5.6";
-    final int SERVER_PORT = 123456;
+    final String SERVER_ADDRESS = "1.2.3.4";
+    final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
         Client client = new Client();
@@ -10,26 +15,25 @@ public class Client {
     }
 
     private void run() {
-        // FIXME : premiere ligne ?
-        try (Socket socket = new Socket("localhost", 1234);
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
              var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
              var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
 
-            // print the supported operations
-            String separator = "/";
-            String add = "ADD";
-            String substract = "SUBSTRACT";
-            String multiply = "MULTIPLY";
-            String divide = "DIVIDE";
-            String instructions = "Enter " + add + separator + substract + separator + multiply + separator + divide + " follow by two numers (int or float) :\n";
+             String separator = "/";
+             String add = "ADD";
+             String substract = "SUBSTRACT";
+             String multiply = "MULTIPLY";
+             String divide = "DIVIDE";
+             String instructions = "Enter " + add + separator + substract + separator + multiply + separator + divide + " follow by two numers (int or float) :\n";
+             System.out.println(instructions);
 
-            // wait for the user to enter a command
-            // FIXME : ???
-            String input = ...
+             Scanner input = new Scanner(System.in);
+             while (input.hasNext()) {
+                 out.write(input.nextLine());
+                 out.flush();
+             }
 
-            // sent the command to the server
-            out.write(contenuEntreParUtilisateur);
-            System.out.println(in.readLine());
+             System.out.println(in.read());
 
         } catch (IOException e) {
             System.out.println("Client: exception while using client socket: " + e);
