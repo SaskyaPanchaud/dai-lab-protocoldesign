cc7623e41c01febc0dd8ec88fe5b155d188e7b0f
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-10-29 12:14:03 +0100

    merge client-SaskyaVersion

--- code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,15 +1,40 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
     final String SERVER_ADDRESS = "1.2.3.4";
     final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
-        // Create a new client and run it
         Client client = new Client();
         client.run();
     }
 
     private void run() {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT); var in = new BufferedReader(new InputStreamReader(socket.getInputStream(),
+                StandardCharsets.UTF_8));
+             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+            String separator = "/";
+            String add = "ADD";
+            String substract = "SUBSTRACT";
+            String multiply = "MULTIPLY";
+            String divide = "DIVIDE";
+            String instructions = "Enter " + add + separator + substract + separator + multiply + separator + divide + " follow by two numers (int or float) :\n";
+            System.out.println(instructions);
+
+            Scanner input = new Scanner(System.in);
+            while (input.hasNext()) {
+                out.write(input.nextLine());
+                out.flush();
+            }
+
+            System.out.println(in.read());
+        } catch (IOException e) {
+            System.out.println("Client: exception while using client socket: " + e);
+        }
     }
 }
