3a49b5bdaab825634d59dcb981f6fccc64b13ae0
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-11-02 13:35:06 +0100

    wip for client

--- code/AlessioGiuliano_SaskyaPanchaud/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/AlessioGiuliano_SaskyaPanchaud/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
+    final String SERVER_ADDRESS = "localhost";
     final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
@@ -15,9 +15,10 @@ public class Client {
     }
 
     private void run() {
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT); var in = new BufferedReader(new InputStreamReader(socket.getInputStream(),
-                StandardCharsets.UTF_8));
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
              var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+
             String separator = "/";
             String add = "ADD";
             String substract = "SUBSTRACT";
@@ -33,6 +34,7 @@ public class Client {
             }
 
             System.out.println(in.read());
+
         } catch (IOException e) {
             System.out.println("Client: exception while using client socket: " + e);
         }
